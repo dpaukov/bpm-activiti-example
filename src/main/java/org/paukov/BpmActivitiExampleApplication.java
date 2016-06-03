5f87a9683d46dff037236248344ae09f6032be4e
dpaukov <d.paukov@gmail.com>
2016-06-02 23:17:02 -0700

    Initial project structure

---
+++ src/main/java/org/paukov/BpmActivitiExampleApplication.java
@@ -0,0 +1,12 @@
+package org.paukov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmActivitiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpmActivitiExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/org/paukov/BpmActivitiExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package org.paukov;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BpmActivitiExampleApplication.class)
+public class BpmActivitiExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

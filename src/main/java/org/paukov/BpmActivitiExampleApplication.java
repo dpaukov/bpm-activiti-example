07f904c53e069ae70eb26b29108d8ebb57e2fa69
dpaukov <d.paukov@gmail.com>
2016-06-07 22:27:39 -0700

    First simple process

--- src/main/java/org/paukov/BpmActivitiExampleApplication.java
+++ src/main/java/org/paukov/BpmActivitiExampleApplication.java
@@ -1,7 +1,12 @@
 package org.paukov;
 
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BpmActivitiExampleApplication {
@@ -9,4 +14,18 @@ public class BpmActivitiExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BpmActivitiExampleApplication.class, args);
 	}
+
+    @Bean
+    public CommandLineRunner init(final RepositoryService repositoryService,
+                                  final RuntimeService runtimeService,
+                                  final TaskService taskService) {
+
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... strings) throws Exception {
+                runtimeService.startProcessInstanceByKey("activitiReview");
+            }
+        };
+
+    }
 }
---
+++ src/main/java/org/paukov/service/StringService.java
@@ -0,0 +1,19 @@
+package org.paukov.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StringService {
+
+    public void method1() {
+        System.out.println("Method1 ...");
+    }
+
+    public void method2() {
+        System.out.println("Method2 ...");
+    }
+
+    public void method3() {
+        System.out.println("Method3 ...");
+    }
+}
